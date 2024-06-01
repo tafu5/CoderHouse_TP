848df9f2748b285f41f2433aa5a27fa5961db594
Vtafura5 <valentintafura@hotmail.com>
2024-06-01 18:41:01 -0300

    Modificacion de requirements.txt y de las funciones functions.py

--- functions.py
+++ functions.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import requests
 import psycopg2
+from config import credentials, client_id, client_secret
 
-
-def request_api(client_id: str, client_secret: str):
+def request_api(**kwargs):
 
     """ Devuelve los datos de la API en formato JSON si la conexión es exitosa 
         De lo contrario devuelve  False """
@@ -19,25 +19,29 @@ def request_api(client_id: str, client_secret: str):
         print('No se pudo conectar a la API')
         data = False
     
-    return data
+    # Guardar data en XCom para usar en la siguiente tarea
+    kwargs['ti'].xcom_push(key = 'raw_data', 
+                           value = data)
 
-
-def normalize_json(data_json: dict) -> pd.DataFrame:
+def normalize_json(**kwargs):
+    
     """ Devuelve el JSON normalizado en formato DataFrame y el timestamp del request.\
         Si el input es False devuelve (False, False) """
+    
+    data_json = kwargs['ti'].xcom_pull(task_ids = 'extract_task', 
+                                       key = 'raw_data')
     if data_json:
         # Normalizo el json
-        df_norm = pd.json_normalize( data_json,
-                                        record_path = ['Entity'],
-                                        meta = ['Header'] )
+        df_norm = pd.json_normalize(data_json,
+                                    record_path = ['Entity'],
+                                    meta = ['Header'] )
         
         # Creo columna 'timestamp' con el timestamp del request
         df_norm['timestamp'] = df_norm['Header'].apply(lambda x: x['timestamp'])
 
-        
         # Elimino la columna Header del df_norm 
         df_norm.drop(columns=['Header'],
-                    inplace=True)
+                     inplace=True)
         
         # Nombre de la columna anidada
         nested_col_name = "Linea.Estaciones"
@@ -50,8 +54,8 @@ def normalize_json(data_json: dict) -> pd.DataFrame:
                                     ignore_index=True) 
         
         # Elimino del df_norm la columna con valores anidados
-        df_norm.drop(columns=[nested_col_name],
-                    inplace=True)
+        df_norm.drop(columns = [nested_col_name],
+                     inplace = True)
         
         # Numero de veces que se debe repetir cada fila de df_norm
         repeats = [len(row) for row in nested_column]
@@ -59,15 +63,15 @@ def normalize_json(data_json: dict) -> pd.DataFrame:
         # Repito cada fila segun corresponda
         df_norm_expanded = pd.concat(
             [pd.DataFrame(row.repeat(rep).reshape(-1, rep).T,
-                        columns=df_norm.columns) 
+                        columns = df_norm.columns) 
                         for row, rep in zip(df_norm.values, repeats)],
-                        ignore_index=True) 
+                        ignore_index = True) 
         
         assert len(df_norm_expanded) == len(unnested_column), "Los df deben tener el mismo numero de filas"
         
         # Concateno los dataframes 'df_norm_expanded' y 'unnested_column'
         df_final = pd.concat([df_norm_expanded, unnested_column],
-                        axis=1)
+                             axis = 1)
         
         # Renombro columnas
         df_final.columns = ['line_id', 'trip_id', 'route_id', 'direction_id', \
@@ -78,28 +82,27 @@ def normalize_json(data_json: dict) -> pd.DataFrame:
         # Cambio orden de columnas, asignando 'timestamp' como la primer columna
         df_final = df_final.loc[:, ['timestamp_event'] + [col for col in df_final if col != 'timestamp_event']]
 
-        
-        # Paso a formato datetime las columnas relacionadas con fechas
-        df_final['timestamp_event'] = pd.to_datetime(df_final['timestamp_event'].astype(int), unit='s')
-        df_final['arrival_time']    = pd.to_datetime(df_final['arrival_time'], unit='s')
-        df_final['departure_time']  = pd.to_datetime(df_final['departure_time'], unit='s')
-        df_final['start_date']      = pd.to_datetime(df_final['start_date'])
+        # Guardo el timestamp en una variable
+        timestamp = df_final['timestamp_event'].unique()[0]
 
         # Elimino duplicados
         df_final = df_final.drop_duplicates()
-        
-        # Guardo el timestamp en una variable
-        timestamp = df_final['timestamp_event'].unique()[0]  
+        df_final = df_final.to_dict('records')
 
         print('Datos normalizados con éxito')
     else:
         print("El input 'data' debe ser tipo DataFrame")
         df_final, timestamp = False, False
-        
-    return df_final, timestamp
     
+    # Guardo los datos procesados en xcom
+    kwargs['ti'].xcom_push(key = 'data_transformed',
+                           value = df_final)
+    # Guardo el timestamp en xcom
+    kwargs['ti'].xcom_push(key = 'timestamp',
+                           value = timestamp)
 
-def database_connection(credentials: dict):
+
+def database_connection():
     
     """ Devuelve la conexión a la base de datos si la misma es exitosa.\
         De lo contrario devuelve False """
@@ -119,14 +122,16 @@ def database_connection(credentials: dict):
     except:
         conn = False
         print('No se ha podido conectar a la base de datos')
-        
+    
     return conn
     
 
-
-def create_table(conn, table_name: str, credentials: dict):
+def create_table(table_name: str, credentials: dict):
     
     """ Crea una tabla en la base de datos si existe una conexión a la base de datos """
+
+    # Conexión a la base de datos
+    conn = database_connection()
 
     # Si existe una conexión a la base de datos
     if conn:
@@ -180,17 +185,34 @@ def create_table(conn, table_name: str, credentials: dict):
         print("Debe contecarse a una base de datos para crear una tabla")
 
 
-def data_ingestion(conn, data: pd.DataFrame, timestamp, table_name: str):
+def data_ingestion(**kwargs):
     
     """ Ingesta datos normalizados a una tabla de la base de datos """
 
+    conn = database_connection()
+    
+    data = kwargs['ti'].xcom_pull(task_ids = 'transform_task', 
+                                  key ='data_transformed')
+    
+    timestamp = kwargs['ti'].xcom_pull(task_ids = 'transform_task', 
+                                       key = 'timestamp')
+    
+    
     if data is not False and conn is not False:
+        data = pd.DataFrame(data)
+
+        # Paso a formato datetime las columnas relacionadas con fechas
+        data['timestamp_event'] = pd.to_datetime(data['timestamp_event'].astype(int), unit='s')
+        data['arrival_time']    = pd.to_datetime(data['arrival_time'], unit='s')
+        data['departure_time']  = pd.to_datetime(data['departure_time'], unit='s')
+        data['start_date']      = pd.to_datetime(data['start_date'])
+        
         # Creo cursor 
         cursor = conn.cursor()
 
         # Obtengo los eventos ya cargados en la tabla
         cursor.execute(f"""
-                        SELECT DISTINCT(timestamp_event) FROM {table_name}  
+                        SELECT DISTINCT(timestamp_event) FROM subte  
                         """)
         
         event_check = cursor.fetchall()
@@ -205,7 +227,7 @@ def data_ingestion(conn, data: pd.DataFrame, timestamp, table_name: str):
 
             # Inserción de valores SQL
             sql = f"""
-                INSERT INTO {table_name} ({columns})
+                INSERT INTO subte ({columns})
                 VALUES ({s}) """
 
             # Valores a ingestar la base de datos
@@ -217,7 +239,7 @@ def data_ingestion(conn, data: pd.DataFrame, timestamp, table_name: str):
                     cursor.execute(sql, val)
                     conn.commit()
 
-                print(f"Se ingresaron {len(data)} registros en la tabla '{table_name}'")
+                print(f"Se ingresaron {len(data)} registros en la tabla 'subte'")
             except:
                 print('Hubo un error al momento de ingresar los datos a la base de datos')
         
@@ -228,16 +250,3 @@ def data_ingestion(conn, data: pd.DataFrame, timestamp, table_name: str):
     
     else:
         print("El input 'data' debe ser tipo DataFrame")
-
-
-def ETL(client_id, client_secret, conn):
-    # Extraction: Request a la API
-    data_json = request_api(client_id, client_secret)
-        
-    # Transformation: Normalido el json y obtengo el timestamp del request
-    data, timestamp = normalize_json(data_json)
-
-    # Loading: Ingesta de datos obtenidos desde la API a la tabla 'subte'
-    data_ingestion(conn, data, timestamp, 'subte')
-
-    return data_json, data 
